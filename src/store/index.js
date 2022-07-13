c489ce0f9f8f52528ed83a256a34cd2b7ca2841e
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2022-07-13 07:04:52 +0100

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import todos from './modukes/todos'
+import { values } from 'core-js/core/array';
+
+
+// load vuex
+Vues.use(Vuex);
+
+// create store 
